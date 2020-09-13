09b9409eef90139224a026b68139f1cd31f4330a
Konstantinos <nikos_kazazis@yahoo.com>
2020-09-13 21:14:13 +0300

    Updates

---
+++ precache-manifest.95c28fcf3cf209e3bececfde503df730.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e09697c66a64a2c5af8060c1228ea523",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "8e385e2470ff4dc8b602",
+    "url": "/covid-tracker/static/css/2.a4567992.chunk.css"
+  },
+  {
+    "revision": "5f0a2cf1c520a1681172",
+    "url": "/covid-tracker/static/css/main.577b1ae0.chunk.css"
+  },
+  {
+    "revision": "8e385e2470ff4dc8b602",
+    "url": "/covid-tracker/static/js/2.12f31527.chunk.js"
+  },
+  {
+    "revision": "ebc70c097554e9e2ef968c42fad2beba",
+    "url": "/covid-tracker/static/js/2.12f31527.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f0a2cf1c520a1681172",
+    "url": "/covid-tracker/static/js/main.3842ca5d.chunk.js"
+  },
+  {
+    "revision": "a2c09be945803fa375fb",
+    "url": "/covid-tracker/static/js/runtime-main.948b6ba1.js"
+  }
+]);
